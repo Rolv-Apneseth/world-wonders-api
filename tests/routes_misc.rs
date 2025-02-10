190e8cdd46c6f46cb7875d29ae18f3bfde9550fb
rolv <rolv.apneseth@gmail.com>
2025-02-10 22:32:55 +0000

    feat: add a simple health check route

--- tests/routes_misc.rs
+++ tests/routes_misc.rs
@@ -1,5 +1,5 @@
 use pretty_assertions::assert_eq;
-use world_wonders_api::METRICS_ROUTE;
+use world_wonders_api::{HEALTH_ROUTE, METRICS_ROUTE};
 
 mod common;
 use common::get_server;
@@ -7,6 +7,11 @@ use common::get_server;
 #[tokio::test]
 async fn test_routes_misc() {
     let server = get_server();
+
+    // HEALTH
+    let response = server.get(HEALTH_ROUTE).await;
+    response.assert_status_ok();
+    response.assert_text_contains("Healthy!");
 
     // 404
     let response = server.get("not-a-route").await;
